3abbc297d436093fd0cc4731f7ab3b685d5c5b83
AnonymousNick23 <seyealade8@gmail.com>
2026-07-26 17:12:14 +0100

    todo class commnent

--- index.js
+++ index.js
@@ -24,6 +24,46 @@ todos.forEach((todo, index) => {
    if (todo.completed) li.classList.add("completed");
 
 
-
+li.innerHTML = `
+<input type=checkbox ${todo.completed ? "checked" : ""} data-index=${index}>
+<span>${todo.text}</span>
+<button class="delete-btn" data-index=${index}>Delete</button>
+`;
+todoList.appendChild(li);
 });
+
+
+// update footer status
+const activecount = todos.filter((t) => !t.completed).length;
+taskCount.textContent = `${activecount} task${activecount !== 1 ? "s" : ""} left`;
+
+
+// save to local storage
+
+localStorage.setItem("todos", JSON.stringify(todos));
+
 }
+// actions
+const addTodo = (text) => {
+  constnewTodo = {
+    id: Date.now(),
+    text: text.trim(),
+    completed : false,
+  };
+  todos.push(newTodo);
+  renderTodos();
+}
+
+
+const toggleToggle = (id) => {
+  todos = todos.map((todo)=> todo.id === id ? {...todo, completed: !todo.completed} : todo);
+}
+
+
+
+
+
+
+
+
+renderTodos()
